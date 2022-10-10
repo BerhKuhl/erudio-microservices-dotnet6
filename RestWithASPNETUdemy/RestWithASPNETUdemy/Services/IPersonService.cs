e4448f0e81f0fffe89fe46b6f357c842256a9895
Bernardo <bernardo@sobit.com.br>
2022-10-10 17:17:08 -0300

    Nivelamento

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        List<Person> FindAll();
+        Person FindByID(long id);
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplamentation.cs
@@ -0,0 +1,64 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplamentation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndget(),
+                FirstName = "Bernardo",
+                LastName = "Kuhl",
+                Address = "Várzea Paulista - São Paulo - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndget(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndget()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
